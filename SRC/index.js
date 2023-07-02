e9dd2012b45005539652003d339d019c3ee3ea18
Federico Sivieri <sivieri.federico@gmail.com>
2023-07-02 09:49:26 +0200

    add es6 import/export modules in index file

--- SRC/index.js
+++ SRC/index.js
@@ -1,12 +1,20 @@
-const express = require('express');
+// const express = require('express');
+// const usersRouters = require('./routers/users/users.router.js');
+// const authorsRouters = require('./routers/authors/authors.router.js');
+// const booksRouters = require('./routers/books/books.router.js')
+// const loansRouters = require('./routers/loans/loans.router.js')
+
+import {express} from 'express';
+import {usersRouters} from './routers/users/users.router.js';
+import {authorsRouters} from './routers/authors/authors.router.js';
+import {booksRouters} from './routers/books/books.router.js';
+import {loansRouters} from './routers/loans/loans.router.js';
+import {utilsMiddleware} from './middleware/utils.middleware.js';
+
 const server = express();
-const usersRouters = require('./routers/users/users.router.js');
-const authorsRouters = require('./routers/authors/authors.router.js');
-const booksRouters = require('./routers/books/books.router.js')
-const loansRouters = require('./routers/loans/loans.router.js')
 
 server.use(express.json());
-server.use(require('./middleware/utils.middleware.js'));
+server.use(utilsMiddleware());
 
 
 server.use('/users', usersRouters);
